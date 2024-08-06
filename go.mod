7adcf01e7af871bc757eea8e60660ec7b1a36979
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-08-06 20:51:29 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ahyalfan/coba-coba-go-modul
+
+go 1.22.5
---
+++ say_hello.go
@@ -0,0 +1,7 @@
+package app_golang_module
+
+// jangan lupa pakaihuruf besar awlanya
+// misal disini kita bikin sebuah method
+func SayHello() string {
+	return "hello world"
+}
