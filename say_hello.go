92513cd5006d13d71fdb656a50d1a71268b758c1
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-08-06 21:10:22 +0700

    rename gomodlue

--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,17 @@
-package app_golang_module
+package coba_coba_golang_modul
 
 // jangan lupa pakaihuruf besar awlanya
 // misal disini kita bikin sebuah method
 func SayHello() string {
 	return "hello world"
 }
+
+// Untuk membuat module baru, kita bisa menggunakan perintah : go mod init nama-module
+// Go-Lang akan secara otomatis membuat file go.mod yang berisikan informasi nama module dan juga versi Go-Lang yang kita gunakan
+
+// Go-Lang terintegrasi baik dengan Git
+// Untuk merilis module, kita hanya perlu membuat Tag di Git
+
+// nah jika ingin bisa terintegrasi bisa tambahkan tag
+// git tag v1.0.0 // usahkan versinya mengugakan awalan v
+// git push origin v1.0.0
